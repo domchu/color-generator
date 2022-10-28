06750c6c3b3117b3e25a5db52d074eff75491921
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-28 23:22:44 +0100

    chore: added the project interface

--- src/App.js
+++ src/App.js
@@ -12,7 +12,27 @@ function App() {
     e.preventDefault();
     console.log("hello world!");
   };
-  return <h2>color generator project</h2>;
+  return (
+    <>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
+import rgbToHex from "../src/Utils";
 
 const SingleColor = () => {
   return <h4>single color</h4>;
