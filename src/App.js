9027066705d943b10458d5c87a6b133a71284018
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-28 23:05:50 +0100

    chore:declare color, error, list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world!");
+  };
   return <h2>color generator project</h2>;
 }
 
