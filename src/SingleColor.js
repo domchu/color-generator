959983873200c55204528b35aea1f7bcd4be6e48
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-29 23:34:28 +0100

    chore:display d hexcolor, bgcolor, percent value

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "../src/Utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>;
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className="percent-value">{weight} %</p>
+      <p className="color-value">{hexValue} </p>
+    </article>
+  );
 };
 
 export default SingleColor;
